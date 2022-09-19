615011c528d2d294d2e6efe3d89beb4fd7e0802d
Benjamin Coleman <benjamin.coleman@uconn.edu>
2022-09-18 21:30:40 -0400

    sketch class refactor

--- app.js
+++ app.js
@@ -14,182 +14,192 @@ import fit from "math-fit";
 import GUI from "lil-gui";
 import GSAP from "gsap";
 
-const gui = new GUI();
+import Sketch from "./app/Sketch";
 
-const IMAGE_ASPECT = 1.7777;
-
-let currImageIndex = 0;
-// this is trash and should be in a class
-let base = null;
-
-const app = new PIXI.Application({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    // backgroundColor: 0x1099bb,
-    view: document.querySelector("#scene"),
-    resolution: window.devicePixelRatio || 1,
-    resizeTo: window,
+new Sketch({
+    debug: true,
+    duration: 7,
 });
 
-const uniforms = {
-    uResolution: new PIXI.Point(
-        window.innerWidth * window.devicePixelRatio,
-        window.innerHeight * window.devicePixelRatio
-    ),
-    uTime: app.uTime,
-    uThreshold: 0.0,
-    // Delete this
-    uScale: new PIXI.Point(1, 1),
-};
+// const gui = new GUI();
 
-gui.add(uniforms, "uThreshold", 0, 1, 0.01);
+// const IMAGE_ASPECT = 1.7777;
 
-// Resizes renderer view in CSS pixels to allow for resolutions other than 1
-// app.renderer.autoDensity = true;
-// Resize the view to match viewport dimensions
-// app.renderer.resize(window.innerWidth, window.innerHeight);
-app.uTime = 0;
+// let currImageIndex = 0;
+// // this is trash and should be in a class
+// let base = null;
 
-const resources = PIXI.Loader.shared.resources;
-const imagePaths = [t1, t2, t3, t4, t5, t6, t7, t8, t9];
-let loadedImages = [];
-const loadingTextures = [];
+// const app = new PIXI.Application({
+//     width: window.innerWidth,
+//     height: window.innerHeight,
+//     // backgroundColor: 0x1099bb,
+//     view: document.querySelector("#scene"),
+//     resolution: window.devicePixelRatio || 1,
+//     resizeTo: window,
+// });
 
-const loadImages = (paths, whenLoaded) => {
-    var imgs = [];
-    paths.forEach(function (path) {
-        var img = new Image();
-        img.onload = function () {
-            imgs.push(img);
-            if (imgs.length == paths.length) whenLoaded(imgs);
-        };
-        img.src = path;
-    });
-};
+// const uniforms = {
+//     uResolution: new PIXI.Point(
+//         window.innerWidth * window.devicePixelRatio,
+//         window.innerHeight * window.devicePixelRatio
+//     ),
+//     uTime: app.uTime,
+//     uThreshold: 0.0,
+//     // Delete this
+//     uScale: new PIXI.Point(1, 1),
+// };
 
-loadImages(imagePaths, (images) => {
-    loadedImages = images;
-    loadedImages.forEach((img) => {
-        let texture = PIXI.Texture.from(img);
-        loadingTextures.push(texture);
-    });
-    base = new PIXI.Sprite(loadingTextures[currImageIndex]);
-    base.filters = [invertFilter];
+// gui.add(uniforms, "uThreshold", 0, 1, 0.01);
 
-    base.anchor.set(0.5);
-    // base.position.set(base.texture.orig.width / 2, base.texture.orig.height / 2);
-    base.x = window.innerWidth / 2;
-    base.y = window.innerHeight / 2;
-    base.width = window.innerWidth;
-    base.height = window.innerHeight;
+// // Resizes renderer view in CSS pixels to allow for resolutions other than 1
+// // app.renderer.autoDensity = true;
+// // Resize the view to match viewport dimensions
+// // app.renderer.resize(window.innerWidth, window.innerHeight);
+// app.uTime = 0;
 
-    app.stage.addChild(base);
-    app.render(base, texture);
-});
+// const resources = PIXI.Loader.shared.resources;
+// const imagePaths = [t1, t2, t3, t4, t5, t6, t7, t8, t9];
+// let loadedImages = [];
+// const loadingTextures = [];
 
-// Load resources, then init the app
-PIXI.Loader.shared.add(["../shaders/invert.fs"]);
+// const loadImages = (paths, whenLoaded) => {
+//     var imgs = [];
+//     paths.forEach(function (path) {
+//         var img = new Image();
+//         img.onload = function () {
+//             imgs.push(img);
+//             if (imgs.length == paths.length) whenLoaded(imgs);
+//         };
+//         img.src = path;
+//     });
+// };
 
-const texture = PIXI.Texture.from(t1);
+// loadImages(imagePaths, (images) => {
+//     loadedImages = images;
+//     loadedImages.forEach((img) => {
+//         let texture = PIXI.Texture.from(img);
+//         loadingTextures.push(texture);
+//     });
+//     base = new PIXI.Sprite(loadingTextures[currImageIndex]);
+//     base.filters = [invertFilter];
 
-// const invertShader = resources["../shaders/invert.fs"].data;
-// Create a new Filter using the fragment shader
-// We don't need a custom vertex shader, so we set it as `undefined`
+//     base.anchor.set(0.5);
+//     // base.position.set(base.texture.orig.width / 2, base.texture.orig.height / 2);
+//     base.x = window.innerWidth / 2;
+//     base.y = window.innerHeight / 2;
+//     base.width = window.innerWidth;
+//     base.height = window.innerHeight;
 
-// const noiseFilter = new PIXI.Filter(undefined, noiseShader, uniforms);
-// base.filters = [noiseFilter];
+//     app.stage.addChild(base);
+//     app.render(base, texture);
+// });
 
-// const noiseTexture = PIXI.RenderTexture.create({ width: 800, height: 800 });
-// const sprite = new PIXI.Sprite(PIXI.Texture.WHITE);
-// const noiseFilter = new PIXI.Filter(undefined, noiseShader, uniforms);
-// sprite.filters = [noiseFilter];
+// // Load resources, then init the app
+// PIXI.Loader.shared.add(["../shaders/invert.fs"]);
 
-// need to update this later to match base
-// sprite.width = window.innerWidth;
-// sprite.height = window.innerHeight;
-// sprite.anchor.set(0.5);
+// const texture = PIXI.Texture.from(t1);
 
-// let renderer = PIXI.autoDetectRenderer();
-// console.log(renderer);
-// renderer.render(sprite, { noiseTexture });
-// app.stage.addChild(sprite);
+// // const invertShader = resources["../shaders/invert.fs"].data;
+// // Create a new Filter using the fragment shader
+// // We don't need a custom vertex shader, so we set it as `undefined`
 
-// uniforms.uNoiseShader = noiseFilter;
+// // const noiseFilter = new PIXI.Filter(undefined, noiseShader, uniforms);
+// // base.filters = [noiseFilter];
 
-const invertFilter = new PIXI.Filter(undefined, fragmentShader, uniforms);
+// // const noiseTexture = PIXI.RenderTexture.create({ width: 800, height: 800 });
+// // const sprite = new PIXI.Sprite(PIXI.Texture.WHITE);
+// // const noiseFilter = new PIXI.Filter(undefined, noiseShader, uniforms);
+// // sprite.filters = [noiseFilter];
 
-// // Assign the filter to the background Sprite
+// // need to update this later to match base
+// // sprite.width = window.innerWidth;
+// // sprite.height = window.innerHeight;
+// // sprite.anchor.set(0.5);
 
-// app.stage.x = window.innerWidth * 0.5;
-// app.stage.y = window.innerHeight * 0.5;
+// // let renderer = PIXI.autoDetectRenderer();
+// // console.log(renderer);
+// // renderer.render(sprite, { noiseTexture });
+// // app.stage.addChild(sprite);
 
-app.ticker.add((delta) => {
-    app.uTime += delta;
-    uniforms.uTime = app.uTime;
-    // this will have to reset after each "slide"
+// // uniforms.uNoiseShader = noiseFilter;
 
-    // should be duration in seconds / seconds passed
-    uniforms.uThreshold += delta;
-    // console.log("uThreshold: ", uniforms.uThreshold);
-});
+// const invertFilter = new PIXI.Filter(undefined, fragmentShader, uniforms);
 
-const incrementSlide = () => {
-    console.log("incrementing slide, ", currImageIndex);
-    // const loadingTextures = [];
-    currImageIndex++;
-    uniforms.uThreshold = 0;
-    base.texture =
-        loadingTextures[(currImageIndex + 1) % loadingTextures.length];
-    // if (incrementSlide >= loadedImages.length - 1) {
-    //     currImageIndex = 0;
-    // } else {
-    //     currImageIndex++;
-    // }
+// // // Assign the filter to the background Sprite
 
-    // base.texture.set(loadedImages[currImageIndex]);
-};
+// // app.stage.x = window.innerWidth * 0.5;
+// // app.stage.y = window.innerHeight * 0.5;
 
-GSAP.set(incrementSlide, {
-    delay: 7,
-    onRepeat: incrementSlide,
-    repeat: -1,
-    repeatDelay: 7,
-});
+// app.ticker.add((delta) => {
+//     app.uTime += delta;
+//     uniforms.uTime = app.uTime;
+//     // this will have to reset after each "slide"
 
-const textureDimensions = {
-    w: 2133,
-    h: 1200,
-};
+//     // should be duration in seconds / seconds passed
+//     uniforms.uThreshold += delta;
+//     // console.log("uThreshold: ", uniforms.uThreshold);
+// });
 
-const resize = () => {
-    let scale = 1;
-    app.renderer.resize(window.innerWidth, window.innerHeight);
-    viewportAspect = window.innerWidth / window.innerHeight;
-    // base.width = window.innerWidth;
-    // base.height = window.innerHeight;
-    if (IMAGE_ASPECT > viewportAspect) {
-        uniforms.uScale.set(IMAGE_ASPECT / viewportAspect, 1);
-        scale = window.innerHeight / base.texture.height;
-    } else {
-        uniforms.uScale.set(1, viewportAspect / IMAGE_ASPECT);
-        scale = window.innerWidth / base.texture.width;
-    }
-    // base.scale.set(scale, scale);
-    // console.log(base, scale);
+// const incrementSlide = () => {
+//     console.log("incrementing slide, ", currImageIndex);
+//     // const loadingTextures = [];
+//     currImageIndex++;
+//     uniforms.uThreshold = 0;
+//     base.texture =
+//         loadingTextures[(currImageIndex + 1) % loadingTextures.length];
+//     resize();
+//     // if (incrementSlide >= loadedImages.length - 1) {
+//     //     currImageIndex = 0;
+//     // } else {
+//     //     currImageIndex++;
+//     // }
 
-    const viewportDimensions = {
-        w: window.innerWidth,
-        h: window.innerHeight,
-    };
+//     // base.texture.set(loadedImages[currImageIndex]);
+// };
 
-    const cover = fit(textureDimensions, viewportDimensions);
-    base.position.set(
-        cover.left + viewportDimensions.w / 2,
-        cover.top + viewportDimensions.h / 2
-    );
-    base.scale.set(cover.scale, cover.scale);
-};
+// GSAP.set(incrementSlide, {
+//     delay: 7,
+//     onRepeat: incrementSlide,
+//     // change this to -1 when ready to repeating
+//     repeat: 1,
+//     repeatDelay: 7,
+// });
 
-window.addEventListener("resize", resize);
+// const textureDimensions = {
+//     w: 2133,
+//     h: 1200,
+// };
 
-app.start();
+// const resize = () => {
+//     let scale = 1;
+//     app.renderer.resize(window.innerWidth, window.innerHeight);
+//     viewportAspect = window.innerWidth / window.innerHeight;
+//     // base.width = window.innerWidth;
+//     // base.height = window.innerHeight;
+//     if (IMAGE_ASPECT > viewportAspect) {
+//         uniforms.uScale.set(IMAGE_ASPECT / viewportAspect, 1);
+//         scale = window.innerHeight / base.texture.height;
+//     } else {
+//         uniforms.uScale.set(1, viewportAspect / IMAGE_ASPECT);
+//         scale = window.innerWidth / base.texture.width;
+//     }
+//     // base.scale.set(scale, scale);
+//     // console.log(base, scale);
+
+//     const viewportDimensions = {
+//         w: window.innerWidth,
+//         h: window.innerHeight,
+//     };
+
+//     const cover = fit(textureDimensions, viewportDimensions);
+//     base.position.set(
+//         cover.left + viewportDimensions.w / 2,
+//         cover.top + viewportDimensions.h / 2
+//     );
+//     base.scale.set(cover.scale, cover.scale);
+// };
+
+// window.addEventListener("resize", resize);
+// resize();
+
+// app.start();
---
+++ app/Sketch.js
@@ -0,0 +1,175 @@
+// Library imports
+import * as PIXI from "pixi.js";
+import GUI from "lil-gui";
+import fit from "math-fit";
+import GSAP from "gsap";
+
+// Texture imports
+import t1 from "../assets/1.jpg";
+import t2 from "../assets/2.jpg";
+import t3 from "../assets/3.jpg";
+import t4 from "../assets/4.jpg";
+import t5 from "../assets/5.jpg";
+import t6 from "../assets/6.jpg";
+import t7 from "../assets/7.jpg";
+import t8 from "../assets/8.jpg";
+import t9 from "../assets/9.jpg";
+
+// Shader imports
+import fragmentShader from "../shaders/fragment.glsl";
+
+const IMAGE_ASPECT = 1.7777;
+const TEXTURE_DIMENSIONS = {
+    w: 2133,
+    h: 1200,
+};
+
+export default class Sketch {
+    constructor(opts) {
+        this.pixi = new PIXI.Application({
+            width: window.innerWidth,
+            height: window.innerHeight,
+            // backgroundColor: 0x1099bb,
+            view: document.querySelector("#scene"),
+            resolution: window.devicePixelRatio || 1,
+            autoDensity: true,
+            // resizeTo: window,
+        });
+        this.debug = opts.debug;
+        this.time = 0;
+        this.uniforms = {
+            uResolution: new PIXI.Point(
+                window.innerWidth * window.devicePixelRatio,
+                window.innerHeight * window.devicePixelRatio
+            ),
+            uTime: this.time,
+            uThreshold: 0.0,
+        };
+        this.textureImages = [t1, t2, t3, t4, t5, t6, t7, t8, t9];
+        this.textures = [];
+        this.sprite = null;
+        this.ticker = this.pixi.ticker;
+        this.duration = opts.duration || 5;
+        this.currImageIndex = 0;
+        this.paused = false;
+
+        this.init(() => {
+            this.attachResize();
+            this.settings();
+            this.resize();
+            this.start();
+            this.pixi.start();
+        });
+    }
+
+    init(cb) {
+        // do we need this?
+        let that = this;
+        const loader = PIXI.Loader.shared;
+        this.textureImages.forEach((url, i) => {
+            loader.add(url);
+        });
+
+        loader.onLoad.add((loader, resource) => {
+            this.textures.push(resource.texture);
+        });
+
+        loader.onComplete.add(() => {
+            if (!that.sprite) {
+                this.addObjects();
+            }
+            cb();
+        });
+
+        loader.load();
+    }
+
+    addObjects() {
+        this.sprite = new PIXI.Sprite(this.textures[this.currImageIndex]);
+        const invertFilter = new PIXI.Filter(
+            undefined,
+            fragmentShader,
+            this.uniforms
+        );
+        this.sprite.filters = [invertFilter];
+        this.sprite.anchor.set(0.5);
+        this.sprite.x = window.innerWidth / 2;
+        this.sprite.y = window.innerHeight / 2;
+        this.sprite.width = window.innerWidth;
+        this.sprite.height = window.innerHeight;
+
+        this.pixi.stage.addChild(this.sprite);
+        this.pixi.render(this.sprite);
+    }
+
+    attachResize() {
+        window.addEventListener("resize", this.resize.bind(this));
+    }
+
+    resize() {
+        console.log(
+            this.pixi.renderer,
+            window.innerHeight,
+            window.innerWidth,
+            window.devicePixelRatio
+        );
+        let scale = 1;
+
+        const viewportDimensions = {
+            w: window.innerWidth,
+            h: window.innerHeight,
+        };
+
+        this.pixi.renderer.resize(window.innerWidth, window.innerHeight);
+        const viewportAspect = window.innerWidth / window.innerHeight;
+        if (IMAGE_ASPECT > viewportAspect) {
+            // uniforms.uScale.set(IMAGE_ASPECT / viewportAspect, 1);
+            scale = window.innerHeight / this.sprite.texture.height;
+        } else {
+            // uniforms.uScale.set(1, viewportAspect / IMAGE_ASPECT);
+            scale = window.innerWidth / this.sprite.texture.width;
+        }
+
+        const cover = fit(TEXTURE_DIMENSIONS, viewportDimensions);
+        this.sprite.position.set(
+            cover.left + viewportDimensions.w / 2,
+            cover.top + viewportDimensions.h / 2
+        );
+        this.sprite.scale.set(cover.scale, cover.scale);
+    }
+
+    settings() {
+        if (this.debug) {
+            this.gui = new GUI();
+            this.gui.add(this, "duration", 0, 10, 1).onChange((val) => {
+                this.duration = val;
+            });
+            this.gui.add(this, "paused").onChange((val) => {
+                this.paused = val;
+            });
+        }
+    }
+
+    start() {
+        // Maybe make this a timeline?
+        GSAP.set(this.incrementSlide.bind(this), {
+            delay: this.duration,
+            onRepeat: this.incrementSlide.bind(this),
+            repeat: this.paused ? 0 : -1,
+            repeatDelay: this.duration,
+        });
+        this.ticker.add((delta) => {
+            this.time += delta;
+            this.uniforms.uTime = this.time;
+            this.uniforms.uThreshold += delta;
+        });
+    }
+
+    incrementSlide() {
+        this.currImageIndex++;
+        this.uniforms.uThreshold = 0;
+        this.sprite.texture =
+            this.textures[(this.currImageIndex + 1) % this.textures.length];
+        this.resize();
+    }
+}
